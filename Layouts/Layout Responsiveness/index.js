618bb6a298e023c412b56dfda945b10a2d30c484
Astrid-Moga <moga.astrid@yahoo.ro>
2021-08-30 12:11:07 +0300

    .

---
+++ Layouts/Layout Responsiveness/index.js
@@ -0,0 +1,10 @@
+function showMenu() {
+    let menu = document.getElementById("mobile-menu")
+    menu.style.display = "initial"
+}
+
+function hideMenu() {
+    console.log("mama")
+    let menu = document.getElementById("mobile-menu")
+    menu.style.display = "none"
+}
